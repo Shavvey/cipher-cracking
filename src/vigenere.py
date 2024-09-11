98afb7c0f3a267fd09cf1beb34836c984967ea63
Cole Johnson <coledjohn@gmail.com>
2024-09-11 17:52:06 -0600

    adding fitness

--- src/vigenere.py
+++ src/vigenere.py
@@ -1,5 +1,6 @@
 from util import ALPHABET, ASCII_TO_ORD, ORD_TO_ASCII
 from math import log
+import string
 
 EXAMPLE = """
 It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their
@@ -46,6 +47,44 @@ def vig_decryption(key: str, ciphertext: str) -> str:
         else:
             plaintext += p
     return plaintext
+
+
+def get_textragrams(text: str) -> list[float]:
+    text = text.lower()
+    text.translate(str.maketrans("", "", string.punctuation))
+    print(f"Final text: {text}")
+    tetrafreq: list[float] = [0.00] * 26 * 26 * 26
+    for i in range(len(text) - 3):
+        x = (
+            ALPHABET.index(text[i]) * 26 * 26 * 26
+            + ALPHABET.index(text[i + 1]) * 26 * 26
+            + ALPHABET.index(text[i + 2]) * 26
+            + ALPHABET.index(text[i + 3])
+        )
+        tetrafreq[x] += 1
+    for i in range(26 * 26 * 26 * 26):
+        tetrafreq[i] = tetrafreq[i] / (len(text) - 3)
+    return tetrafreq
+
+
+def fitness(text):
+    tetrafrequencies = get_textragrams(text)
+    result = 0
+    for i in range(len(text) - 3):
+        tetragram = text[i : i + 4]
+        x = (
+            ALPHABET.index(tetragram[0]) * 26 * 26 * 26
+            + ALPHABET.index(tetragram[1]) * 26 * 26
+            + ALPHABET.index(tetragram[2]) * 26
+            + ALPHABET.index(tetragram[3])
+        )
+        y = tetrafrequencies[x]
+        if y == 0:
+            result += -15  # some large negative number
+        else:
+            result += log(y)
+    result = result / (len(text) - 3)
+    return result
 
 
 def main():
