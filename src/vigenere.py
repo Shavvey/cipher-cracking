e068d5bc40c8f14ba24664cb1116bafcfd7e7cb1
Cole Johnson <coledjohn@gmail.com>
2024-09-11 17:34:18 -0600

    awful

--- src/vigenere.py
+++ src/vigenere.py
@@ -1,4 +1,21 @@
 from util import ALPHABET, ASCII_TO_ORD, ORD_TO_ASCII
+from math import log
+
+EXAMPLE = """
+It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their
+first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal
+secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station
+with enough power to destroy an entire planet. Pursued by the Empire's sinister agents,
+Princess Leia races home aboard her starship, custodian of the stolen plans that can save her
+people and restore freedom to the galaxy. It is a dark time for the Rebellion. Although the
+Death Star has been destroyed, Imperial troops have driven the Rebel forces from their
+hidden base and pursued them across the galaxy. Evading the dreaded Imperial
+Starfleet, a group of freedom fighters led by Luke Skywalker has established a new secret base
+on the remote ice world of Hoth. The evil lord Darth Vader, obsessed with finding young
+Skywalker, has dispatched thousands of remote probes into the far reaches of space. Luke
+Skywalker has returned to his home planet of Tatooine in an attempt to rescue his friend Han
+Solo from the clutches of the vile gangster Jabba the Hutt
+"""
 
 
 def vig_encrpytion(key: str, plaintext: str) -> str:
@@ -31,25 +48,8 @@ def vig_decryption(key: str, ciphertext: str) -> str:
     return plaintext
 
 
-AMBROSIA_PLAINTEXT = """
-It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their
-first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal
-secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station
-with enough power to destroy an entire planet. Pursued by the Empire's sinister agents,
-Princess Leia races home aboard her starship, custodian of the stolen plans that can save her
-people and restore freedom to the galaxy. It is a dark time for the Rebellion. Although the
-Death Star has been destroyed, Imperial troops have driven the Rebel forces from their
-hidden base and pursued them across the galaxy. Evading the dreaded Imperial
-Starfleet, a group of freedom fighters led by Luke Skywalker has established a new secret base
-on the remote ice world of Hoth. The evil lord Darth Vader, obsessed with finding young
-Skywalker, has dispatched thousands of remote probes into the far reaches of space. Luke
-Skywalker has returned to his home planet of Tatooine in an attempt to rescue his friend Han
-Solo from the clutches of the vile gangster Jabba the Hutt
-"""
-
-
 def main():
-    ciphertext = vig_encrpytion("hellothere", AMBROSIA_PLAINTEXT)
+    ciphertext = vig_encrpytion("hellothere", EXAMPLE)
     print(ciphertext)
     plaintext = vig_decryption("hellothere", ciphertext)
     print(plaintext)
