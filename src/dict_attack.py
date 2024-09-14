9aa74934a018df76f435cdab0170d0997426d154
Cole Johnson <coledjohn@gmail.com>
2024-09-14 10:25:31 -0600

    changing out example

--- src/dict_attack.py
+++ src/dict_attack.py
@@ -3,7 +3,25 @@ import vigenere as vig
 
 
 EXAMPLE = """
-Or glw jwmmaravy, Msq gjmsziq xzm zkeiifa sth glw msxxu. Xzm wgvgl oik cmglgcl lseq svv bsvh, svv jeeofmky anw gdwx xui xiuk ss xzm vkic. Efl lni Ftazaz ss Kgl ogw usnmjort snmj zlr jskw uj glw esziew. Svv Msq wsqv, “Rig xzmjk fr paozz,” eah lpwxi jek tamlg. Efl Yuh feo bzgx glw tamlg asa yusq. Efl Yuh fihijgxrh lpw rmtll njuq glw lsxoaika. Yuh pedtwj xui dqynx Qeq, ifj xui vijqrrwk pw ieypwl Fokux. Svv zlrvw esy iiifqfm eah lpwxi jek ugxrvry, bzk jvvkb vgc. Nrv Ogj wnmv, “Twz xuijm tk ea ipxstwr mf bzk qvhkb gl xui oilkvf, efl dkx vx kmhgvnxw bzk anxwzk lvbq lpw cegija.” Sth Tsv usji glw mpveaww ifj wrtszsziq xzm ogxrvk bzgx jijm mthrv lpw kbcefaw lvbq lpw cegija lneg awzw gfbzw bzk iktsvkk. Eah ab ogw fs. Svv Msq gstdkh glw mpveaww Pwgzrr. Svv zlrvw esy iiifqfm eah lpwxi jek ugxrvry, bzk wrggvv jel. Efl Yuh feal, “Dkx glw esziew mvvkv glw pwgzrrk jw meglwzwj xbkwbzkv vrlw gti cpskw, grq pwb lni qvq tsth nthmsx.” Eah ab ogw fs. Ywv ieypwl lni qvq tsth Rejbz, grq xzm ogxrvk bzgx jijm ygxuijmv zstilpwx lr gstdkh Fisa. Sth Tsv asc xuel ql cef kgwv.
+Or glw jwmmaravy, Msq gjmsziq xzm zkeiifa sth glw msxxu. 
+Xzm wgvgl oik cmglgcl lseq svv bsvh, svv jeeofmky anw gdwx 
+xui xiuk ss xzm vkic. Efl lni Ftazaz ss Kgl ogw usnmjort snmj 
+zlr jskw uj glw esziew. Svv Msq wsqv, “Rig xzmjk fr paozz,” 
+eah lpwxi jek tamlg. Efl Yuh feo bzgx glw tamlg asa yusq. 
+Efl Yuh fihijgxrh lpw rmtll njuq glw lsxoaika. 
+Yuh pedtwj xui dqynx Qeq, ifj xui vijqrrwk pw ieypwl Fokux. 
+Svv zlrvw esy iiifqfm eah lpwxi jek ugxrvry, bzk jvvkb vgc. 
+Nrv Ogj wnmv, “Twz xuijm tk ea ipxstwr mf bzk qvhkb gl xui oilkvf, 
+efl dkx vx kmhgvnxw bzk anxwzk lvbq lpw cegija.” 
+Sth Tsv usji glw mpveaww ifj wrtszsziq xzm ogxrvk bzgx
+jijm mthrv lpw kbcefaw lvbq lpw cegija lneg awzw gfbzw bzk 
+iktsvkk. Eah ab ogw fs. Svv Msq gstdkh glw mpveaww Pwgzrr. 
+Svv zlrvw esy iiifqfm eah lpwxi jek ugxrvry, bzk wrggvv jel. 
+Efl Yuh feal, “Dkx glw esziew mvvkv glw pwgzrrk jw meglwzwj 
+xbkwbzkv vrlw gti cpskw, grq pwb lni qvq tsth nthmsx.” 
+Eah ab ogw fs. Ywv ieypwl lni qvq tsth Rejbz, 
+grq xzm ogxrvk bzgx jijm ygxuijmv zstilpwx lr gstdkh Fisa. 
+Sth Tsv asc xuel ql cef kgwv.
 """
 
 
